7fa482f3cc82248189405e6d0e01eacf96793d68
Zewotr <zewotrl@gmail.com>
2026-06-08 20:24:43 +0300

    test(cases): for eeach loan type

---
+++ cypress/e2e/home-loan-happy-path.cy.js
@@ -0,0 +1,21 @@
+describe('Home Loan Happy Path (Salaried with Co-Applicant)', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.fixture('valid-home-loan').as('loanData');
+  });
+
+  it('should complete a home loan application with co-applicant', function() {
+    cy.fillStep1(this.loanData.step1.loanType, this.loanData.step1.amount, this.loanData.step1.tenure, this.loanData.step1.purpose);
+    cy.fillStep2(this.loanData.step2);
+    cy.fillStep3(this.loanData.step3.pan, this.loanData.step3.aadhaar, this.loanData.step3.consent);
+    cy.fillStep4(this.loanData.step4);
+    cy.fillStep5(this.loanData.step5.employmentType, this.loanData.step5);
+    cy.fillStep6(this.loanData.step6);
+    cy.fillStep7(this.loanData.documents);
+    cy.fillStep8();
+
+    cy.contains('Application Submitted').should('be.visible');
+    cy.contains('Application Reference Number:').should('be.visible');
+    cy.get('[data-testid="close-modal"]').click();
+  });
+});

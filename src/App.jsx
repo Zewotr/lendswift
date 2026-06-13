997f5c2a0212966edbfd33017dad7a35269c3dc3
Zewotr <zewotrl@gmail.com>
2026-06-13 21:45:32 +0300

    fix: add layout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import Wizard from './components/wizard/Wizard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  return <Wizard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply" element={<Wizard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
